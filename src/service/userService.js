f6b962a507d02bb7344eb562f21b952a35da3f26
Lazygreed <lazygreed07@gmail.com>
2026-04-25 12:17:16 -0300

    feat: add CreateUserService

---
+++ src/service/userService.js
@@ -0,0 +1,24 @@
+import { v4 as uuidv4 } from 'uuid'
+import bcrypt from 'bcrypt'
+import { PostgresCreateUserRepository } from '../repository/postgres/userRepository.js'
+
+export class CreateUserService {
+   async execute(createUserParams) {
+      // TODO: verificar se o email já existe
+
+      const userID = uuidv4()
+      const hashedPassword = await bcrypt.hash(createUserParams.password, 10)
+
+      const user = {
+         ...createUserParams,
+         ID: userID,
+         password: hashedPassword,
+      }
+
+      const postgresCreateUserRepository = new PostgresCreateUserRepository()
+
+      const createdUser = await postgresCreateUserRepository.execute(user)
+
+      return createdUser
+   }
+}
