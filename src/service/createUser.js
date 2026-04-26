ac9f31684d117cdda173407cecef2ec563132ccd
Lucas Sousa <lucas.sousadev07@gmail.com>
2026-04-26 15:13:10 -0300

    feat: ensure EmailAlreadyInUseError is used

--- src/service/createUser.js
+++ src/service/createUser.js
@@ -2,18 +2,18 @@ import { v4 as uuidv4 } from 'uuid'
 import bcrypt from 'bcrypt'
 import { PostgresCreateUserRepository } from '../repository/postgres/createUser.js'
 import { PostgresGetUserByEmailRepository } from '../repository/postgres/getUserByEmail.js'
+import { EmailAlreadyInUseError } from '../errors/user.js'
 
 export class CreateUserService {
    async execute(createUserParams) {
       const postgresGetUserByEmailRepository =
          new PostgresGetUserByEmailRepository()
 
-      const userWithProvidedEmail = await postgresCreateUserRepository.execute(
-         createUserParams.email,
-      )
+      const userWithProvidedEmail =
+         await postgresGetUserByEmailRepository.execute(createUserParams.email)
 
       if (userWithProvidedEmail)
-         throw new Error('The provided email is already in use.')
+         throw new EmailAlreadyInUseError(createUserParams.email)
 
       const userID = uuidv4()
       const hashedPassword = await bcrypt.hash(createUserParams.password, 10)
