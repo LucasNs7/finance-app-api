88b0cc57a885635c15fc7e050d2289cc256c1479
Lucas Sousa <lucas.sousadev07@gmail.com>
2026-04-26 14:41:37 -0300

    feat: add validator of UUID

--- src/controllers/getUserById.js
+++ src/controllers/getUserById.js
@@ -1,9 +1,18 @@
-import { notFound, ok } from './helpers.js'
+import { badRequest, notFound, ok } from './helpers.js'
 import { GetUserByIdService } from '../service/getUserById.js'
+import validator from 'validator'
 
 export class GetUserByIdController {
    async execute(httpRequest) {
       try {
+         const isValidId = validator.isUUID(httpRequest.params.userId)
+
+         if (!isValidId) {
+            return badRequest({
+               message: 'The provided ID is not valid',
+            })
+         }
+
          const getUserByIdService = new GetUserByIdService()
 
          const user = await getUserByIdService.execute(
