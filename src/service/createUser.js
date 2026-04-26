939be46503d00345930bc290bb3d378d8fa461c7
Lucas Sousa <lucas.sousadev07@gmail.com>
2026-04-26 14:57:26 -0300

    feat: add GetUserByEmailRepository

--- src/service/createUser.js
+++ src/service/createUser.js
@@ -1,9 +1,20 @@
 import { v4 as uuidv4 } from 'uuid'
 import bcrypt from 'bcrypt'
 import { PostgresCreateUserRepository } from '../repository/postgres/createUser.js'
+import { PostgresGetUserByEmailRepository } from '../repository/postgres/getUserByEmail.js'
 
 export class CreateUserService {
    async execute(createUserParams) {
+      const postgresGetUserByEmailRepository =
+         new PostgresGetUserByEmailRepository()
+
+      const userWithProvidedEmail = await postgresCreateUserRepository.execute(
+         createUserParams.email,
+      )
+
+      if (userWithProvidedEmail)
+         throw new Error('The provided email is already in use.')
+
       const userID = uuidv4()
       const hashedPassword = await bcrypt.hash(createUserParams.password, 10)
 
