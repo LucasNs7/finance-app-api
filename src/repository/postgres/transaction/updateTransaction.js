55f573281335617a62be824e7e86a5ca99f0da38
Lucas Sousa <lucas.sousadev07@gmail.com>
2026-04-29 10:49:12 -0300

    feat: add PostgresUpdateTransactionRepository

---
+++ src/repository/postgres/transaction/updateTransaction.js
@@ -0,0 +1,26 @@
+import { PostgresHelper } from "../../../db/postgres/helper.js"
+
+export class PostgresUpdateTransactionRepository {
+   async execute(userId, updateParams) {
+      const updateFields = []
+      const updateValues = []
+
+      Object.keys(updateParams).forEach((key) => {
+         updateFields.push(`${key} = $${updateValues.length + 1}`)
+         updateValues.push(updateParams[key])
+      })
+
+      updateValues.push(userId)
+
+      const updateQuery = `
+            UPDATE transactions
+            SET ${updateFields.join(', ')}
+            WHERE ID = $${updateValues.length}
+            RETURNING *
+         `
+
+      const updateTransaction = await PostgresHelper.query(updateQuery, updateValues)
+
+      return updateTransaction[0]
+   }
+}
