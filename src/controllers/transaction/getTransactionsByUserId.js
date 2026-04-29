1abe4b7eb266cf32138aa9bf8136a3a459d950d0
Lucas Sousa <lucas.sousadev07@gmail.com>
2026-04-29 09:44:45 -0300

    feat: add GetTransactionsByUserIdController

--- src/controllers/transaction/getTransactionsByUserId.js
+++ src/controllers/transaction/getTransactionsByUserId.js
@@ -1,4 +1,10 @@
 import { UserNotFoundError } from '../../errors/user.js'
+import {
+   checkIfIdIsValid,
+   invalidIdResponse,
+   requiredFieldIsMissingResponse,
+} from '../helpers/generic.js'
+import { ok } from '../helpers/http.js'
 import { serverError, userNotFoundResponse } from '../index.js'
 
 export class GetTransactionsByUserIdController {
@@ -8,10 +14,24 @@ export class GetTransactionsByUserIdController {
 
    async execute(httpRequest) {
       try {
-         const params = httpRequest.body
+         const userId = httpRequest.query.userId
+
+         if (!userId) return requiredFieldIsMissingResponse('userId')
+
+         const userIdIsValid = checkIfIdIsValid(userId)
+
+         if (!userIdIsValid) return invalidIdResponse()
+
+         const transactions = this.getTransactionByUserIdService.execute({
+            userId,
+         })
+
+         return ok(transactions)
       } catch (error) {
          console.error(error)
+
          if (error instanceof UserNotFoundError) return userNotFoundResponse()
+
          return serverError()
       }
    }
